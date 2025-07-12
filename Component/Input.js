5e9241ca2b236b034e7866278996d17ee524b177
meethatkar <meethatkar000@gmail.com>
2025-07-12 20:09:42 +0530

    intial commit

---
+++ Component/Input.js
@@ -0,0 +1,56 @@
+"use client"
+import Cookies from 'js-cookie';
+import React, { useState } from 'react'
+
+const Input = (props) => {
+
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("");
+
+  const sendTaskDets = (event) => {
+    event.preventDefault();
+    // console.log(title, desc);
+    if (title.length == 0) {
+      console.log("blank");
+      document.querySelector("#title>.error-div").style.display = "block";
+      // document.querySelector("#title>.error-div").style.paddingTop = "24px";
+
+    }
+    if (desc.length == 0) {
+      console.log("desc blank");
+      document.querySelector("#description>.error-div").style.display = "block";
+      // document.querySelector("#description>.error-div").style.paddingTop = "24px";
+
+    }
+    else {
+      document.querySelector("#description>.error-div").style.display = "none";
+      document.querySelector("#title>.error-div").style.display = "none";
+
+      // COOKIES TRYING
+      // Cookies.set('task-dets', JSON.stringify({ title, desc }), { expires: 7 });
+
+      props.sendChildData({ title, desc })
+      settitle("");
+      setdesc("");
+    }
+  }
+
+  return (
+    <div id="input-wrapper" className='mt-4'>
+      <form className='flex-col flex md:flex-row gap-5 md:gap-22 items-center' onSubmit={sendTaskDets}>
+        <div id="title">
+          <input className='text-lg md:text-2xl border-2 border-gray-400 px-3 py-1.5 rounded-lg text-center focus:border-gray-800' type="text" placeholder='Enter Task Title' value={title} onChange={(event) => { settitle(event.target.value) }} />
+          <div className='error-div text-red-600 font-semibold hidden'> Task Title is Required </div>
+        </div>
+        <div id="description">
+          <input className='text-lg md:text-2xl border-2 border-gray-400 px-3 py-1.5 rounded-lg text-center md:w-[120%] focus:border-gray-800' type="text" placeholder='Enter Task Description' value={desc} onChange={(event) => { setdesc(event.target.value) }} />
+          <div className='error-div text-red-600 font-semibold hidden'> Task Description is Required </div>
+        </div>
+        <button className='md:ml-10 px-3 bg-blue-500 text-white font-semibold w-fit text-md md:text-xl rounded-lg  md:h-12 hover:cursor-pointer'> Add Task </button>
+
+      </form>
+    </div>
+  )
+}
+
+export default Input
---
+++ app/page.js
@@ -0,0 +1,99 @@
+"use client"
+import Input from '@/Component/Input'
+import Cookies from 'js-cookie';
+import React, { useEffect, useState } from 'react'
+
+const page = () => {
+
+
+  const [mainTask, setmainTask] = useState([]);
+
+  const getChildData = (childData) => {
+    setmainTask([childData, ...mainTask]);
+
+    // COOKIES TRYING
+    // let cookieData = JSON.parse(Cookies.get('task-dets'));
+    // console.log(cookieData);
+    // setmainTask([...mainTask,cookieData]);
+
+  }
+
+  let renderTask = <p className='text-center text-xl font-medium'> No task Added !! </p>
+  let renderStateMob = <p className='text-center text-xl font-medium'> No task Added !! </p>
+
+  // COOKIES TRYING
+  // useEffect(() => {
+  //   console.log("mainTask updated:", mainTask);
+  //   Cookies.set("main-tasks",mainTask);
+  // }, [mainTask]);
+
+
+  if (mainTask.length > 0) {
+
+    /*
+    slice() makes a copy of the array so the original mainTask is not mutated.
+    reverse() just changes the order to show latest tasks first.
+    */
+    renderTask = mainTask.map((task, index) => {
+      return <li className='grid grid-cols-[80px_200px_1fr_150px_150px] text-lg gap-4 mb-5' key={index}>
+        {/* key acts like ID, if compulosory in react, as react identifies each unquie li using key only. */}
+        <p> {index + 1} </p>
+        <p>  {task.title} </p>
+        <p> {task.desc} </p>
+        <button className='bg-green-400 w-1/2 h-fit text-white rounded-lg hover:cursor-pointer' onClick={(event) => { completeTask(event.target) }}> Done </button>
+        <button className='bg-red-400 w-1/2  h-fit rounded-lg hover:cursor-pointer' onClick={() => { deleteTask(index) }}>Delete</button>
+      </li>
+    })
+
+    renderStateMob = mainTask.map((task,index)=>{
+      return <div className="task pb-3 border-b-1 border-b-gray-400 flex flex-col" key={index}>
+            <h3 className='text-2xl font-medium'> Task {index+1} </h3>
+            <div id="title-mob" className='flex gap-4'> <span className='min-w-[105px] font-medium'> Title:- </span>  <h5> {task.title} </h5> </div>
+            <div id="desc-mob" className='flex gap-4'> <span className='min-w-[100px] font-medium'> Description:- </span>  <h5> {task.desc} </h5> </div>
+            <button className='mr-5 px-3 py-0.5 bg-green-400 text-white rounded-lg mt-5 w-fit' onClick={(event) => { completeTask(event.target) }}> check </button>
+            <button className='mr-5 px-3 py-0.5 bg-red-400 text-white rounded-lg mt-5 w-fit' onClick={() => { deleteTask(index) }}> Delete </button>
+          </div>
+    })
+  }
+
+  const deleteTask = (index) => {
+    // alert("Are you sure, you want to delete it");
+    let clonedTaskArr = [...mainTask];
+    clonedTaskArr.splice(index, 1);
+    setmainTask(clonedTaskArr);
+  }
+
+  const completeTask = (eventDiv) => {
+    let liDiv = eventDiv.parentNode;
+    liDiv.children[0].textContent = "✅  " + liDiv.children[0].textContent;
+    liDiv.style.color = "green";
+  }
+
+  return (
+    <div id="wrapper" className='md:bg-gradient-to-br md:from-[#CFB908] md:via-[#39A68B] md:to-[#1339B9] min-h-screen w-screen flex items-center justify-center '>
+      <div id="container" className='bg-white w-screen min-h-[100%] md:h-auto md:w-10/12 md:w-3/4 py-6 px-8 rounded-xl text-black md:shadow-md shadow-black/70 flex flex-col items-center gap-5'>
+        <img src="./Screenshot 2025-07-11 235240.png" alt="" className='h-18 md:h-25' />
+        <h1 className='text-2xl md:text-4xl font-medium'> Todo List App</h1>
+        <p className='-mt-3 text-gray-500 text-lg text-center '> Create your Task(s) and get organised today </p>
+        <Input sendChildData={getChildData}></Input>
+        {/* <Input></Input> */}
+        <div id="task-dets" className='hidden md:block w-11/12 bg-gray-100/60  px-4 py-4 mt-5 shadow-xs shadow-gray-500/60 rounded-lg'>
+          <div id="heading" className='grid grid-cols-[80px_200px_1fr_150px_150px] text-xl gap-4 font-medium pb-2 mb-8 border-b-2 border-gray-500'>
+            <p> Sr.no </p>
+            <p> Title  </p>
+            <p> Description </p>
+            <p> Mark Done </p>
+            <p> Delete 🗑️ </p>
+          </div>
+          {renderTask}
+        </div>
+
+        <div id="mob-task" className='md:hidden flex flex-col item-start w-full h-full gap-5'>
+          {renderStateMob}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
